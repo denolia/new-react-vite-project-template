ae61e3376773f9e84fd46b17c24a99ccbf945759
Julia Bubnova <bubnova.j.i@gmail.com>
2023-04-30 14:09:31 +0200

    Import env vars

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import React from "react";
 
+import { API } from "./environment";
 import type { Foo } from "./Foo"; // testing consistent type imports
 
 export function App() {
   const foo: Foo = "asdf";
   console.log(foo); // needed to test eslint warnings
+  console.log(API);
 
   return <div>Hello</div>;
 }
